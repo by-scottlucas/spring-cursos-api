a0f79402bdf6c96db8f3d09325c05dbf8ffecc07
Lucas S <Lucasluke307@gmail.com>
2024-02-26 15:01:43 -0300

    Aplicando validações

--- src/main/java/br/com/api/controllers/CursoController.java
+++ src/main/java/br/com/api/controllers/CursoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.models.Curso;
 import br.com.api.repositorys.CursoRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
+@Validated
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/cursos")
@@ -34,27 +39,37 @@ public class CursoController {
 
     @PostMapping()
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Curso create(@RequestBody Curso curso) {
+    public Curso create(@RequestBody @Valid Curso curso) {
         return cursoRepository.save(curso);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Curso> read(@PathVariable Long id) {
+    public ResponseEntity<Curso> read(@PathVariable @NotNull @Positive Long id) {
         return cursoRepository.findById(id)
                 .map(response -> ResponseEntity.ok().body(response))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
-    @ResponseStatus(code = HttpStatus.CREATED)
-    public Curso update(@PathVariable Long id, @RequestBody Curso curso) {
-        return cursoRepository.save(curso);
+    public ResponseEntity<Curso> update(@PathVariable @NotNull @Positive Long id, @RequestBody Curso curso) {
+        return cursoRepository.findById(id)
+                .map(response -> {
+                    response.setNome(curso.getNome());
+                    response.setCategoria(curso.getCategoria());
+                    Curso update = cursoRepository.save(response);
+                    return ResponseEntity.ok().body(update);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
-        cursoRepository.deleteById(id);
+    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
+        return cursoRepository.findById(id)
+                .map(response -> {
+                    cursoRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
--- src/main/java/br/com/api/models/Curso.java
+++ src/main/java/br/com/api/models/Curso.java
@@ -1,11 +1,16 @@
 package br.com.api.models;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -17,10 +22,16 @@ public class Curso {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nm_curso")
+    @NotBlank()
+    @Length(min = 5, max = 100)
+    @NotNull(message = "O campo precisa ser preenchido.")
+    @Column(length = 100, nullable = false)
     private String nome;
 
-    @Column(name = "ctg_curso")
+    @NotNull()
+    @Length(max = 10)
+    @Pattern(regexp = "Back-End|Front-End")
+    @Column(length = 10, nullable = false)
     private String categoria;
 
 }
