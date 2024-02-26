12c8c4422e4e25f400943a448ed7774fc79b4f13
Lucas Santos Silva <lucas.santoss@ibm.com>
2024-02-26 11:55:28 -0300

    Implementando métodos em CursoController

--- src/main/java/br/com/api/controllers/CursoController.java
+++ src/main/java/br/com/api/controllers/CursoController.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,15 +20,15 @@ import br.com.api.models.Curso;
 import br.com.api.repositorys.CursoRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/cursos")
-@CrossOrigin("http://localhost:4200")
-public class CursosController {
+public class CursoController {
 
     @Autowired
     private CursoRepository cursoRepository;
 
     @GetMapping()
-    public @ResponseBody List<Curso> list() {
+    public List<Curso> list() {
         return cursoRepository.findAll();
     }
 
@@ -42,6 +43,18 @@ public class CursosController {
         return cursoRepository.findById(id)
                 .map(response -> ResponseEntity.ok().body(response))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Curso update(@PathVariable Long id, @RequestBody Curso curso) {
+        return cursoRepository.save(curso);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        cursoRepository.deleteById(id);
     }
 
 }
